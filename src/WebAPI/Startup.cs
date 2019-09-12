39d4cf33f1b57813beca2cd68dc477584fbdd618
Juan Bulfon <bulfonjf@gmail.com>
2019-09-12 18:05:30 -0300

    WebAPI was changed to netcore 2.2

--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -1,16 +1,23 @@
-using ApplicationServices.Interfaces;
+﻿using ApplicationServices.Interfaces;
 using ApplicationServices.Services;
 using AutoMapper;
 using BlogRepository;
 using BlogRepository.Contexts;
 using BlogRepository.Interfaces;
+using WebAPI.MiddleWares;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using WebAPI.MiddleWares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace WebAPI
 {
@@ -26,16 +33,17 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
             var connectionString = Configuration["ConnectionStrings:MySQLConnectionString"];
             services.AddDbContext<BlogContext>(o => o.UseMySQL(connectionString));
             RegisterAutomapperDependencies(services);
             services.AddScoped<IBlogServices, BlogServices>();
             services.AddScoped<IBlogRepository, MySQLBlogRepository>();
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,19 +54,11 @@ namespace WebAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseMiddleware<ExceptionMiddleware>();
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseAuthentication();
+            //app.UseHttpsRedirection();
+            app.UseMvc();
         }
 
         private static void RegisterAutomapperDependencies(IServiceCollection services)
